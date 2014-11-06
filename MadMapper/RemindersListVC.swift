a31ab3571a2cfbbf8c918557ae88f6595339dd0a
Casey White <casey.r.white@gmail.com>
2014-11-06 01:06:09 -0800

    Add boiler plate from Apple for updating tableView on fetchedResultsController events; update tableView constraints; remove custom message when asking user to allow location services

--- MadMapper/RemindersListVC.swift
+++ MadMapper/RemindersListVC.swift
@@ -7,15 +7,111 @@
 //
 
 import UIKit
+import CoreData
 
-class RemindersListVC: UIViewController {
+class RemindersListVC: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var managedObjectContext: NSManagedObjectContext!
+    var fetchedResultsController: NSFetchedResultsController!
+    
+    let _dateFormatter = NSDateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        self.managedObjectContext = appDelegate.managedObjectContext
+        
+        self._dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "didGetCloudChanges:", name: NSPersistentStoreDidImportUbiquitousContentChangesNotification, object: appDelegate.persistentStoreCoordinator)
+        
+        self.tableView.dataSource = self
+        
+        let fetchRequest = NSFetchRequest(entityName: "Reminder")
+        let createdDateSortDescriptor = NSSortDescriptor(key: "createdDate", ascending: true)
+        fetchRequest.sortDescriptors = [createdDateSortDescriptor]
+        
+        self.fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: nil, cacheName: "Reminders")
+        self.fetchedResultsController.delegate = self
+        
+        var error: NSError? = nil
+        if !self.fetchedResultsController.performFetch(&error) {
+            println("Error fetching reminders: \(error?.localizedDescription)")
+        }
+    }
+    
+    func dateFormatter() -> NSDateFormatter {
+        return self._dateFormatter
     }
 
+    func didGetCloudChanges(notification: NSNotification) {
+        self.managedObjectContext.mergeChangesFromContextDidSaveNotification(notification)
+    }
+    
+    // MARK: - Table View
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.fetchedResultsController.fetchedObjects?.count ?? 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("ReminderCell") as ReminderCell
+        self.configureCell(cell, atIndexPath: indexPath)
+        return cell
+    }
+    
+    func configureCell(cell: ReminderCell, atIndexPath indexPath: NSIndexPath) {
+        if let reminder = self.fetchedResultsController.fetchedObjects?[indexPath.row] as? Reminder {
+            cell.reminderNameLabel.text = reminder.name
+            cell.createdDateLabel.text = self.dateFormatter().stringFromDate(reminder.createdDate)
+        }
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            let context = self.fetchedResultsController.managedObjectContext
+            context.deleteObject(self.fetchedResultsController.objectAtIndexPath(indexPath) as NSManagedObject)
+            
+            var error: NSError? = nil
+            if !context.save(&error) {
+                println("Error deleting reminder: \(error?.localizedDescription)")
+            }
+        }
+    }
+
+    
+    // MARK: - NSFetchedResultsControllerDelegate
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        self.tableView.beginUpdates()
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        switch type {
+        case .Insert:
+            self.tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+        case .Delete:
+            self.tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+        case .Update:
+            let cell = self.tableView.cellForRowAtIndexPath(indexPath!) as ReminderCell
+            self.configureCell(cell, atIndexPath: indexPath!)
+        case .Move:
+            self.tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+            self.tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+        default:
+            return
+        }
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        self.tableView.endUpdates()
+    }
 
 }
